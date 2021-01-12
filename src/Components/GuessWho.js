089025d52db9cf8ccbc7884a03cbf93f68995520
Nourajaloud <75969098+Nourajaloud@users.noreply.github.com>
2021-01-12 11:51:44 +0300

    GuessWho

--- src/Components/GuessWho.js
+++ src/Components/GuessWho.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import UserContext from './UserContext'
 import { Link } from 'react-router-dom'
 import InfoModal from './InfoModal'
+import '../styles/card.css'
+import logo from '../logo.svg'
 
 class GuessWho extends Component {
     static contextType = UserContext;
@@ -9,7 +11,9 @@ class GuessWho extends Component {
         guessWho: [],
         items: null,
         index: -1,
+        zIndex: "",
         display: '',
+        descDisplay: "",
         modalShow: false
     }
 
@@ -23,9 +27,23 @@ class GuessWho extends Component {
     }
 
     //next guess button
+    // handleClick = e => {
+    //     e.preventDefault()
+
+    //     this.setState(prevState => {
+    //         let display = prevState.display
+    //         if (prevState.index === prevState.guessWho.length) {
+    //             display = "none"
+    //         }
+    //         return {
+    //             index: prevState.index + 1,
+    //             display: display
+    //         }
+    //     })
+
+    // }
     handleClick = e => {
         e.preventDefault()
-
         this.setState(prevState => {
             let display = prevState.display
             if (prevState.index === prevState.guessWho.length) {
@@ -33,10 +51,18 @@ class GuessWho extends Component {
             }
             return {
                 index: prevState.index + 1,
-                display: display
+                display: display,
+                thirdClass: "animate__animated animate__fadeOutTopRight animate__faster",
+                zIndex: 9,
+                descDisplay: "none"
             }
         })
-
+        setTimeout(() => {
+            this.setState({
+                thirdClass: "animate__animated animate__fadeInTopRight animate__faster",
+                zIndex: 100
+            })
+        }, 300)
     }
 
     setModalShow = (toggleShow) => {
@@ -47,18 +73,55 @@ class GuessWho extends Component {
         // const [modalShow, setModalShow] = React.useState(false);
         let output
         output = this.state.guessWho.map((item, index) => {
-            return <div key={index}> {item.name} <img src={item.img} /> </div>
+            return <div key={index}>
+                <img className="card-img" src={item.img} />
+
+                <div>{item.name}</div>
+            </div>
         })
         return (
             <>
+
                 <div className="row justify-content-start">
+                    <button className="info-btn main-games green col-1 text-right" onClick={() => this.setModalShow(true)}>اعرفنا اكثر</button>
+                    <Link className="return-btn col-6" to="/guess"> تبي ترجع؟</Link>
+                </div>
+
+                <h1 className="first-do-title">حزر فزر :</h1>
+                <div className="cardContainer row justify-content-center" >
+                    <div className="card" >
+                        <p style={{ display: this.state.descDisplay }}>التعليمات:</p>
+                        <p style={{ display: this.state.descDisplay }}>
+                            مافيها شروط بس حاول تلمح لخويك او للباقين مين شفت وخلهم يجيبونها صح.
+
+                        </p>
+
+                    </div>
+                    <div className={`secound secound-img ${this.state.thirdClass}`} style={{ zIndex: this.state.zIndex }}>
+                        <p>  {output[this.state.index]} </p>
+
+                        <img src={logo} className="card-ship guess-ship" alt="اللوقو" />
+
+                    </div>
+                </div>
+                <button className="hvr-bob game-buttons" onClick={this.handleClick} style={{ display: this.state.display }}>التالي</button>
+
+                {/* <div className="row justify-content-start">
                     <button className="info-btn col-1 text-right" onClick={() => this.setModalShow(true)}>اعرفنا اكثر</button>
                     <Link to="/guess" className="return-btn">تبي ترجع</Link>
                 </div>
-                <button onClick={this.handleClick} style={{ display: this.state.display }}>التالي</button>
-                <div>
-                    {output[this.state.index]}
+
+                <h1 className="first-do-title"> حزر فزر</h1>
+                <div className="cardContainer row justify-content-center" >
+                    <div className="card ">
+                        {output[this.state.index]}
+                        <img src={logo} className="card-ship" alt="اللوقو" />
+                    </div>
+
                 </div>
+                <button className="hvr-bob game-buttons" onClick={this.handleClick} style={{ display: this.state.display }}>
+                    التالي
+                </button> */}
 
                 <InfoModal
                     show={this.state.modalShow}
